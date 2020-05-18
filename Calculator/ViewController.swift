e932b69754f5b74b258bcf2e2ba52c7534b0a0e4
ald_23 <aldiyaar23@gmail.com>
2020-05-18 23:28:34 +0600

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Aldiyar Massimkhanov on 5/18/20.
+//  Copyright © 2020 Aldiyaar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
